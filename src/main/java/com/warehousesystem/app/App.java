9b99812687c89d94afbddd933f4838e30ac19aff
tssvett <tssvett@mail.ru>
2024-04-13 23:38:42 +0300

    Added profiling. Two profiles are available: local and default. Wrap the password and port in environment variables

--- src/main/java/com/warehousesystem/app/App.java
+++ src/main/java/com/warehousesystem/app/App.java
@@ -1,8 +1,6 @@
 package com.warehousesystem.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class App {
