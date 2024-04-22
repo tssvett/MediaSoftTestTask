3f4870ffd1d6de765e352d01cb2e01f773017b98
tssvett <tssvett@mail.ru>
2024-04-22 22:24:19 +0300

    fixed imports

--- src/main/java/com/warehousesystem/app/model/WarehouseGood.java
+++ src/main/java/com/warehousesystem/app/model/WarehouseGood.java
@@ -2,8 +2,18 @@ package com.warehousesystem.app.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.warehousesystem.app.converter.DateConverter;
-import jakarta.persistence.*;
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Convert;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
